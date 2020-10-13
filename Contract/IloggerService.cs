a167a9ba7219c961117f29dd4435700f87c42719
aleksandarruba <aleksandarruba@live.com>
2020-10-13 08:51:33 +0200

    Add project files.

---
+++ Contract/IloggerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Contract
+{
+    public interface IloggerService
+    {
+        public void LogInfo(string message);
+        public void LogWarn(string message);
+        public void LogDebug(string message);
+        public void LogError(string message);
+    }
+}
