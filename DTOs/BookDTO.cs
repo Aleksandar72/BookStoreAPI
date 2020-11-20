8da9817b0275a73ec77a5e0948cc9768cd7cd9e9
aleksandarruba <aleksandarruba@live.com>
2020-11-20 13:53:40 +0100

    Toast messages and finish book CRUD

--- DTOs/BookDTO.cs
+++ DTOs/BookDTO.cs
@@ -39,6 +39,8 @@ namespace BookStoreAPI.DTOs
         [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
+
+        public string Isbn { get; set; }
         
         [StringLength(500)]
         public string Summary { get; set; }
